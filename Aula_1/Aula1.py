5615f1cc4d472aa66fcbceed76112b50d001a7e7
jhulius <melojuliano@gmail.com>
2025-02-24 14:43:33 -0300

    Aula 1 curso de python via udemy

---
+++ Aula_1/Aula1.py
@@ -0,0 +1,7 @@
+#incio de programa - Comentario simples
+# command + / -> comenta a linha
+'''
+aspas triplas comentam multiplas linhas
+Assim como esse comentario
+'''
+print('hello')
---
+++ Aula_1/variaveis.py
@@ -0,0 +1,17 @@
+# variaveis primitivas
+
+idade = 32 # int
+peso = 80.4 #float
+nome = 'Juliano' #String
+ativo = True #boolean
+resultado = None #none = ausencia de valor
+
+print(type(idade))
+print(type(peso))
+print(type(nome))
+print(type(ativo))
+
+#concatenando variaveis
+#escrendo a frase 'Meu nome é Juliano e tenho 32 anos
+print('Meu nome é ', nome , 'e tenho ', idade, 'anos' )
+
